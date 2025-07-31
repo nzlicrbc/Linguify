1bbff3544891ed0fd9516515e91f3e8d8300f59e
nzlicrbc <nzlcn10@hotmail.com>
2025-07-31 20:52:21 +0300

    feat: add basic streak data model and constants

--- app/src/main/java/com/example/linguify/utils/Constants.kt
+++ app/src/main/java/com/example/linguify/utils/Constants.kt
@@ -2,4 +2,9 @@ package com.example.linguify.utils
 
 object Constants {
     const val PREFERENCES_NAME = "linguify_preferences"
+    const val STREAK_DAYS_COUNT = 7
+    const val DATE_FORMAT = "yyyy-MM-dd"
+    const val KEY_STREAK_DAYS = "streak_days"
+    const val KEY_LAST_ACTIVITY_DATE = "last_activity_date"
+    const val KEY_CURRENT_STREAK = "current_streak"
 }
